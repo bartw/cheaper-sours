31ce74927719b7a08c8be1ce2fb6ef1d054322ab
Bart Wijnants <bartwijnants@outlook.com>
2017-11-03 09:33:24 +0100

    consume socket

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
+  const socket = new WebSocket("ws://" + document.location.host);
+  socket.onopen = () => {
+    socket.send("Ping");
+  };
+  socket.onmessage = function(event) {
+    console.log(event.data);
+  };
   return <h1>Hearse-corpus</h1>;
 }
